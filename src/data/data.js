dfd3a0f618d7bdc9efbdaf5a73e1849839fcafd2
Bruno <ti.brunomachado@gmail.com>
2025-02-11 17:41:42 -0300

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    slug: "feijao",
+    title: "Feijão",
+    content:
+      "Feijão é um nome comum para uma grande variedade de sementes de plantas de alguns gêneros da família Fabaceae. Proporciona nutrientes essenciais como proteínas, ferro, cálcio, vitaminas, carboidratos e fibras.",
+  },
+  {
+    slug: "mocoto",
+    title: "Mocotó",
+    content:
+      "O mocotó ou mão-de-vaca, é um prato tradicional baseado em patas cozidas sem casco ou extremidades de bovinos.",
+  },
+];
