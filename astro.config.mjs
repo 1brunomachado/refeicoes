adb461993af40f51294984c9497f624b8d09e762
Bruno <ti.brunomachado@gmail.com>
2025-03-05 10:58:34 -0300

    style: index style, in summary

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: "192.168.15.87",
+  },
+});
