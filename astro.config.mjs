db97a716f630b189c994f20dc9eb4b1ee6f1ce61
Bruno <ti.brunomachado@gmail.com>
2025-03-06 08:38:48 -0300

    style: a lot

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: "192.168.15.87",
-  },
+  base: "/hs/refeicoescongeladas/",
 });
